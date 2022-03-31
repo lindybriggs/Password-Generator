a227e2321559894a5a0ced38649fa2fd20e9e90a
Lindy Briggs <briggs.lindy@yahoo.com>
2022-03-31 17:08:46 -0500

    lowercase functioning

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,3 +11,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+let alphabet = "abcdefghijklmnopqrstuvwxyz"
+let alphabetArray = "abcdefghijklmnopqrstuvwxyz".split("")
+let string = "";
+
+for (let i = 0; i < 10; i++) {
+  let randomNumber = Math.random()*alphabetArray.length
+  let roundedNumber = Math.floor(randomNumber)
+
+  string += alphabetArray[roundedNumber]
+}
+
+console.log(string);
